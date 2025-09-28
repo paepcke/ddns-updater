8c57b4aeecc38a8678d1472075b83153af479a76
Andreas Paepcke <paepcke@cs.stanford.edu>
2025-09-28 10:41:18 -0700

    Added more unittests to test_dns_service.py

--- src/lanmanagement/test/test_dns_service.py
+++ src/lanmanagement/test/test_dns_service.py
@@ -4,11 +4,14 @@
 # @Date:   2025-09-27 17:22:28
 # @File:   /Users/paepcke/VSCodeWorkspaces/ddns-updater/src/lanmanagement/test/test_dns_service.py
 # @Last Modified by:   Andreas Paepcke
-# @Last Modified time: 2025-09-27 17:28:25
+# @Last Modified time: 2025-09-28 10:37:39
 #
 # **********************************************************
 
 import unittest
+from unittest.mock import MagicMock, patch
+
+from dns.resolver import Answer
 
 from lanmanagement.dns_service import DNSService
 
@@ -19,12 +22,40 @@ class TestDNSService(unittest.TestCase):
 
     #----------- Tests ------------
 
-    def test_get_a_records_from_server(self):
+    @patch('lanmanagement.dns_service.dns.resolver.Resolver')    
+    def test_get_a_records_from_server(self, mock_resolver_class):
 
-        host = 'dresl.net'
+        host = 'myhost.net'
         nameserver = 'dns1.registrar-servers.com'
-        ip_addrs = DNSService.get_a_records_from_server(host, nameserver)
-        print(ip_addrs)
+
+        # Create a mock instance of the mocked Resolver:
+        mock_resolver_instance = MagicMock()
+        
+        # Have the constructor of the mocked Resolver class
+        # return a mock_resolver_instance, instead of a 
+        # real one:
+        mock_resolver_class.return_value = mock_resolver_instance
+
+        # Create mock DNS record objects
+        mock_dns_record = MagicMock()
+        # The code to be mocked in the code to be tested 
+        # inside the get_A_records() method is:
+        #    result = resolver.resolve(domain_or_host, 'A')
+        #    return [str(rdata) for rdata in result]
+        # The resolver.resolve() method returns list 
+        # (i.e. 'result') on which __str__() method is 
+        # called in the list comprehension. Give the
+        # mock_dns_record a __str__() method:
+        
+        mock_dns_record.__str__ = lambda self: '192.168.1.100'
+
+        # Mock the resolve() method on our mock Resolver class
+        # to return a list of mock_dns_records
+
+        mock_resolver_instance.resolve.return_value = [mock_dns_record]
+
+        ip_addrs = DNSService.get_A_records(host, nameserver)
+        self.assertListEqual(ip_addrs, ['192.168.1.100'])
 
 # ------------------------ Main ------------
 if __name__ == '__main__':
